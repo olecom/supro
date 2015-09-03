3c641a539c8f4c31cd3c6c7556934d65479da3a3
Oleg Verych <ole4at@gmail.com>
2015-09-03 14:06:11 +0300

    _http: add more error checks; remove from Agent; fix messages
    Signed-off-by: Oleg Verych <ole4at@gmail.com>

--- _http.js
+++ _http.js
@@ -4,14 +4,18 @@ var http = require('http')
 ctl = http.get(// requesting info from remote api
     process.argv[2],// 'url:port/cmd'
     getData
-);
+)
+
 ctl.setTimeout(process.argv[3] || 2048)// timeout
+ctl.on('socket', function(socket){ socket.emit('agentRemove')})
 ctl.on('error', ret_data)
 ctl.end()
 
 function getData(ctlres){
     res = [], len = res.length
     ctlres.on('data', get_chunk)// collecting data chunks
+    ctlres.on('aborted', ret_data)
+    ctlres.on('error', ret_data)
     ctlres.on('end', ret_data)// end of processing
 }
 function get_chunk(chunk){
@@ -19,9 +23,10 @@ function get_chunk(chunk){
     res.push(chunk)
 }
 function ret_data(e){
-    e && console.log('error: ', e)
+    e && console.log('!Error _http.js: ', e)
     setImmediate(function(){
-        console.log('_http res: ', Buffer.concat(res, len).toString())
+        console.log('_http res:')
+        console.log(Buffer.concat(res, len).toString())
         res = void 0
         process.exit(e ? 1 : 0)
     }, 64)

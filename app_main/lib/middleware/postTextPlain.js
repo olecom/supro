d330b6bc6e0d7a37629b8e607df7e2c1e9d8129a
Oleg Verych <ole4at@gmail.com>
2015-09-03 11:25:14 +0300

    mw postTextPlain: robust and optimized rewrite
    For real world use better get https://github.com/stream-utils/raw-body
    
    Signed-off-by: Oleg Verych <ole4at@gmail.com>

--- app_main/lib/middleware/postTextPlain.js
+++ app_main/lib/middleware/postTextPlain.js
@@ -1,22 +1,50 @@
-function postTextPlain(req, res, next){
-    if (req._body) return next()
-    var limit = require('connect').limit('4mb')
-       ,utils = require('connect/lib/utils.js')
-       ,mime = utils.mime(req)
-    if('text/plain' == mime || 'application/javascript' == mime)
-        return limit(req, res
-    ,function(err){
-        if(err) return next(err)
-        var buf = ''
-        req.setEncoding('utf8')
-        req.on('data', function(chunk){ buf += chunk })
-        req.on('end', function(){
-            req.txt = buf
-            next()
-        })
-        return req._body = true
-    })
-    return next()
-}
+/*
+ * Handle request body of 'text/plain' || 'application/javascript'
+ * For real world use better get https://github.com/stream-utils/raw-body
+ */
+var limit, utils
 
-module.exports = postTextPlain
+limit = require('connect').limit('4mb')
+utils = require('connect/lib/utils.js')
+
+module.exports = function postTextPlain(req, res, next){
+var mime, buf, len
+
+    if(!req._body){
+        mime = utils.mime(req)
+        if('text/plain' == mime || 'application/javascript' == mime){
+            req._body = true
+            buf = [], len = 0
+            return limit(req, res, collect)
+        }
+    }
+    return next()
+
+    function collect(err){
+        if(err) return next(err)
+
+        req.on('data', data)
+        req.on('aborted', end)
+        req.on('close', end)
+        req.on('error', end)
+        req.on('end', end)
+    }
+    function data(chunk){
+        buf.push(chunk)
+        len += chunk.length
+    }
+    function end(err){
+        if(err) log('!postTextPlain: ', err)
+
+        req.removeListener('data', data)
+        req.removeListener('aborted', end)
+        req.removeListener('close', end)
+        req.removeListener('error', end)
+        req.removeListener('end', end)
+
+        req.txt = Buffer.concat(buf, len).toString()
+        buf = void 0
+
+        next()
+    }
+}

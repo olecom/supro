olecom/supro

commit ad9eb39a76a880737f18d5ce1ab442319b0731ab

fix #21 config.oem: replace titles, banners with OEM config in app.htm, l10n
Signed-off-by: Oleg Verych <olecom@gmail.com>

app_main/app.htm
@@ -131,7 +131,7 @@ l10n.errun_file + (errEvent.filename || '"app_front.js" || "App.js"') + '<br><br
     </script>
     <!-- script type="text/javascript" src="test.js" charset="utf-8"></script-->
 </head>
-<body style="overflow:hidden;color:black;background:#FFFFFF url(css/supro.png) center no-repeat;margin:0;height:100%">
+<body style="overflow:hidden;color:black;background:#FFFFFF url(/css/supro.png) center no-repeat;margin:0;height:100%">
 <center id='startup'>
 
 <noscript><br/>

app_main/lib/app.js
@@ -11,6 +11,7 @@ var api      = require('./api.js')
    ,_404     = require('./middleware/404.js')
    ,connect  = api.connect = require('connect')
    ,app      = api.app = connect()
+   ,rootPrefix = "", l10nFiles = [ ]
    ,mwConfig, Modules
 
     // provide api to assign final handlers
@@ -44,6 +45,7 @@ var api      = require('./api.js')
     require('../../app_modules/')(cfg, api)
 
     /* backend static files for HTTP users */
+    cfg.oem && oem_files()
     app.use('/', connect['static'](__dirname + '/../', {
         index: cfg.extjs.loadMiniInit ? 'app-mini.htm' : 'app.htm'
     }))
@@ -66,6 +68,124 @@ var api      = require('./api.js')
 
     return
 
+    // apply oem config (see 'cfg_default.js') to files with text to change
+    function oem_files(){
+    var f, i, c, k, s, fs = require('fs')
+
+        rootPrefix = (s = require('path').normalize(
+            __dirname + '/../../' + (cfg.data || 'data/')
+        )) + (cfg.__name || '') + '_'
+        // check data dir
+        f = "D"
+        try {
+            if(fs.statSync(s).isDirectory()){
+                f = ""// OK
+            } else {
+                log('!ERROR data dir is not a directory')
+                process.exit(1)
+            }
+        } catch(ex){ }
+        // create if needed
+        if(f) try {
+            fs.mkdirSync(s)
+        } catch(ex){
+            log('!ERROR creating data dir')
+            process.exit(1)
+        }
+if(cfg.oem.htm){
+        // read 'app.htm'
+        f = __dirname + '/../app.htm'
+        s = fs.readFileSync(f).toString()
+        // replace oem strings
+        cfg.oem.htm.title && (s = s.replace(/<title>[^<]*<[/]title>/g, '<title>'+ cfg.oem.htm.title +'</title>'))
+        cfg.oem.htm.logo && (s = s.replace(/url[(][/]css[/]supro.png[)]/, 'url('+ cfg.oem.htm.logo +')'))
+        if(cfg.oem.htm.icon){
+            s = s.replace(/<!-- "Yellow[^>]*>/g,'')// remove credit
+            s = s.replace(/<link rel="icon"[^>]*>/g,'<link rel="icon" href="' + cfg.oem.htm.icon + '"/>')
+        }
+        // compare old and new files
+        f = rootPrefix + 'app.htm'
+        try {
+            c = fs.readFileSync(f).toString()
+        } catch(ex) { }
+        // write to /data dir if changed
+        if(c !== s){
+            log('^ Write new OEM "app.htm"')
+            fs.writeFileSync(f, s)
+        }
+
+        // read 'app-mini.htm'
+        // NOTE: copy + paste of the above
+        f = __dirname + '/../app-mini.htm'
+        s = ""
+        try{
+            s = fs.readFileSync(f).toString()
+        } catch(ex){ }
+    if(s){
+        // replace oem strings
+        cfg.oem.htm.title && (s = s.replace(/<title>[^<]*<[/]title>/, '<title>'+ cfg.oem.htm.title +'</title>'))
+        cfg.oem.htm.logo && (s = s.replace(/url[(][/]css[/]supro.png[)]/, 'url('+ cfg.oem.htm.logo +')'))
+        if(cfg.oem.htm.icon){
+            s = s.replace(/<!-- "Yellow[^>]*>/,'')// remove credit
+            s = s.replace(/<link rel="icon"[^>]*>/,'<link rel="icon" href="' + cfg.oem.htm.icon + '"/>')
+        }
+        // compare old and new files
+        f = rootPrefix + 'app-mini.htm'
+        try {
+            c = fs.readFileSync(f).toString()
+        } catch(ex) { }
+        // write to /data dir if changed
+        if(c !== s){
+            log('^ Write new OEM "app-mini.htm"')
+            fs.writeFileSync(f, s)
+        }
+    }
+}
+
+if(cfg.oem.l10n){
+        // l10n
+        k = Object.keys(cfg.oem.l10n)
+        for(i = 0; i < k.length; ++i){
+            f = __dirname + '/../l10n/' + k[i] + '.js'
+            s = fs.readFileSync(f).toString()
+            c = cfg.oem.l10n[k[i]].welcome
+            c && (s = s.replace(/welcome: '[^']*/, "welcome:'" + c))
+            c = cfg.oem.l10n[k[i]].app
+            c && (s = s.replace(/app: '[^']*/, "app:'" + c))
+
+            // compare old and new files
+            f = rootPrefix + k[i] + '.js'
+            try {
+                c = fs.readFileSync(f).toString()
+            } catch(ex) { }
+            // write to /data dir if changed
+            if(c !== s){
+                log('^ Write new OEM l10n "'+ k[i] +'.js"')
+                fs.writeFileSync(f, s)
+            }
+            l10nFiles.push('/l10n/' + k[i] +'.js')
+        }
+}
+        // send them if requested in mw stack from /data dir
+        app.use('/', mwRootDir)
+    }
+
+    function mwRootDir(req, res, next){
+        if('/' == req.url){
+            return sendFile(rootPrefix + (
+                cfg.extjs.loadMiniInit ? 'app-mini.htm' : 'app.htm'
+            ), true)(req, res, next)
+        }
+        if('/app.htm' == req.url){
+            return sendFile(rootPrefix + 'app.htm', true)(req, res, next)
+        }
+
+        if(~l10nFiles.indexOf(req.url)){
+            return sendFile(rootPrefix + req.url.slice(6), true)(req, res, next)
+        }
+        return next()
+    }
+
     function set_mwConfig(mw){
         mwConfig = mw || addModulesConfigs
 
@@ -81,7 +201,7 @@ var api      = require('./api.js')
                     if(k.css) for(j = 0; j < k.css.length; ++j){
                         cfg.extjs.launch.css.push(k.css[j])
                     }
-                    if(k.js)  for(j = 0; j < k.js.length; ++j){
+                    if(k.js) for(j = 0; j < k.js.length; ++j){
                         cfg.extjs.launch.js .push(k.js[j])
                     }
                     cfg.extjs.modules[i] = { extjs:{ }}

config/cfg_default.js
@@ -10,6 +10,28 @@ config = {
     data: 'data/',// directory for application data; defaults to 'data/'
     log: 'log/',
 
+    /* parse, serve from '/data/' directory 'app.htm', 'app-mini.htm' and
+     * `l10n` files with replaced strings from this OEM config
+
+    oem:{
+        htm:{// no localization applied
+            icon: '/css/userman/shutdown.png',// URL
+            logo: '/css/suproem.png',// URL
+            title: 'SUPRO OEM v000&copy;2015-??-??'
+        },
+        l10n:{
+            en:{
+                welcome: 'OEM SUPRO welcomes: application development',
+                app: 'OEM SUPRO Demo'
+            },
+            ru:{
+                welcome: 'Вас приветствует OEM СУПРО!',
+                app: 'OEM СУПРО Демо'
+            }
+        }
+    },
+     */
+
     //TODO: uid gid new ids for process after start or partial init
     modules:{// cfg for stack of things from 'app_modules'
     // order matters: before auth module there are no restrictions to config

commit f296441f23102701c5423512d905f2151ec72752

config oem: implement `nw.js` case for l10n and logo
move background setup out of htm and put into JS to prevent glitches
from no oem to oem config while loading

new old logo url like 'url(css/supro.png)' (no front slash)
fixes: ad9eb39a76a880737f18d5ce1ab442319b0731ab

Signed-off-by: Oleg Verych <ole4at@gmail.com>

app_main/app.htm
@@ -133,7 +133,7 @@ l10n.errun_file + (errEvent.filename || '"app_front.js" || "App.js"') + '<br><br
     </script>
     <!-- script type="text/javascript" src="test.js" charset="utf-8"></script-->
 </head>
-<body style="overflow:hidden;color:black;background:#FFFFFF url(/css/supro.png) center no-repeat;margin:0;height:100%">
+<body style="overflow:hidden;color:black;background:#FFFFFF center no-repeat;margin:0;height:100%">
 <center id='startup'>
 
 <noscript><br/>

app_main/app_front.js
@@ -441,7 +441,22 @@ var cfg, fs = require('fs')
         connectjs: app.versions.connectjs,
         nw: app.process.versions['node-webkit']
     }
-    con.log('reading config: ' + cfg + ' done')
+    con.log(''
++'nw.js: reading config: ' + cfg + '\n'
++'nw.js: loading configured `l10n`: ' + app.cfg.lang
+    )
+
+    if(app.cfg.oem){
+    // "D:/_Projects/supro-olecom/app_main/app.htm" ->
+    // "D:/_Projects/supro-olecom/data/cfg_mini_[ htm || js]"
+        cfg = global.require.main.filename.replace(
+            /[/]app.htm$/,
+            '/../data/' + (app.cfg.__name || '') + '_'
+        )
+        app.cfg.oem.htm.title && (doc.title = app.cfg.oem.htm.title.replace(/&copy;/, '©'))
+    } else {// normal not OEM case
+        cfg = 'l10n/'
+    }
 
     fs = doc.head.getElementsByTagName('script')
     doc.head.removeChild(fs[0])

app_main/app_front_http.js
@@ -6,7 +6,8 @@
 ;(function app_front_http(url){
 var xhr = new XMLHttpRequest
 
-    xhr.open('GET', (url || '') + '/app.config.extjs.json', true)
+    url = url || ''
+    xhr.open('GET', url + '/app.config.extjs.json', true)
     xhr.onreadystatechange = load_config_then_check_ExtJS
     xhr.send()
     xhr = null
@@ -42,7 +43,14 @@ var xhr = new XMLHttpRequest
             + '-' + (extjs_config.loadMiniInit ? 'nw' : 'debug')
             + '.js'
             )
-
+            // setup OEM backgound here to prevent glitches
+            if(extjs_config.oem && extjs_config.oem.htm){
+                extjs_config.oem.htm.logo && (
+                    document.body.style.backgroundImage = 'url('+ url +'/'+ extjs_config.oem.htm.logo +')'
+                )
+            } else {
+                document.body.style.backgroundImage = 'url(css/supro.png)'
+            }
             if(url){
             // `nw` context
                 url = App.cfg.extjs.path// flip path

app_main/lib/app.js
@@ -99,7 +99,7 @@ if(cfg.oem.htm){
         s = fs.readFileSync(f).toString()
         // replace oem strings
         cfg.oem.htm.title && (s = s.replace(/<title>[^<]*<[/]title>/g, '<title>'+ cfg.oem.htm.title +'</title>'))
-        cfg.oem.htm.logo && (s = s.replace(/url[(][/]css[/]supro.png[)]/, 'url('+ cfg.oem.htm.logo +')'))
+        cfg.oem.htm.logo && (s = s.replace(/url[(]css[/]supro.png[)]/, 'url('+ cfg.oem.htm.logo +')'))
         if(cfg.oem.htm.icon){
             s = s.replace(/<!-- "Yellow[^>]*>/g,'')// remove credit
             s = s.replace(/<link rel="icon"[^>]*>/g,'<link rel="icon" href="' + cfg.oem.htm.icon + '"/>')
@@ -169,6 +169,8 @@ if(cfg.oem.l10n){
 }
         // send them if requested in mw stack from /data dir
         app.use('/', mwRootDir)
+        // send oem config into ui for background reset if needed
+        cfg.oem && (cfg.extjs.oem = cfg.oem)
     }
 
     function mwRootDir(req, res, next){
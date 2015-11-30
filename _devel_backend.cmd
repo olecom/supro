2152bc87085201d6358992d182744c5174290707
Oleg Verych <olecom@gmail.com>
2015-11-30 17:34:07 +0300

    _devel_backend.cmd: add info about git under MS
    Signed-off-by: Oleg Verych <olecom@gmail.com>

--- _devel_backend.cmd
+++ _devel_backend.cmd
@@ -1,3 +1,3 @@
 @set CYGWIN=nodosfilewarning
-sh _devel_backend.sh %1 || bin\sh.exe _devel_backend.sh %1
+sh _devel_backend.sh %1 || bin\sh.exe _devel_backend.sh %1 || echo . && echo No POSIX Shell found! && echo Use git-for-windows.github.io for development under MS Windows(R)
 pause
